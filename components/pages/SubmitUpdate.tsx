bd72faa848a270e17a5aa372b568b3b3c0b6cd37
abolimundral5@gmail.com <32287237+manjushakarpe1982@users.noreply.github.com>
2026-04-21 16:20:54 +0530

    Update SubmitUpdate.tsx

--- components/pages/SubmitUpdate.tsx
+++ components/pages/SubmitUpdate.tsx
@@ -6,5 +6,6 @@ import SubmitUpdateImpl from '@/components/reports/SubmitUpdate'
 interface Props { role: Role; go: (p: PageName) => void; flash: (m: string) => void }
 
 export default function SubmitUpdate({ flash }: Props) {
+
   return <SubmitUpdateImpl flash={flash} />
 }
